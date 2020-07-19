c8809a1b155f6df54fcf96d2befd4b151d26a59c
hajar nasr <hajar-nasr@hotmail.com>
2020-07-19 16:06:38 +0200

    .

--- backend/settings.py
+++ backend/settings.py
@@ -161,14 +161,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', "static")
 
 STATICFILES_DIRS = [
     #os.path.join(BASE_DIR, 'build', "static"),
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
 
 MY_EMAIL = 'voilamagicmail@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
